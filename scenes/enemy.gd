37841b17837ba8ef047a5d8ec1d20a438122a226
Zakaria Farah <zakaria.farah55@outlook.com>
2025-10-09 19:31:45 +0300

    wip

--- scenes/enemy.gd
+++ scenes/enemy.gd
@@ -1,19 +1,37 @@
 class_name Enemy extends CharacterBody2D
 @onready var animated_sprite : AnimatedSprite2D = $CharacterBody2D/AnimatedSprite2D
 @onready var nav : NavigationAgent2D = $"../NavigationAgent2D"
-@onready var nav_region: NavigationRegion2D = get_node("/root/Main/TileMap/NavigationRegion2D")
+@onready var tileMap: TileMap = $"../../TileMap"
 @export var speed = 1
 @onready var label = $"../Label"
 var idle : bool = true
 var state = { "idle": false, "wandering": false }
 var target :Vector2
+var walkable_cells: Array[Vector2i] = []
+
+func _ready():
+ nav.path_desired_distance = 100
+ nav.target_desired_distance = 30
+ nav.set_navigation_layer_value(0, 1)
+ for cell in tileMap.get_used_cells(0):
+  var tile_data = $TileMap.get_cell_tile_data(0, cell)
+  if tile_data:
+   for layer_index in range(tile_data.get_navigation_layers_count()):
+	var hello = 1
+	
+func get_random_walkable_point() -> Vector2:
+ if walkable_cells.size() == 0:
+  return Vector2.ZERO
+ var cell = walkable_cells[randi() % walkable_cells.size()]
+ return tileMap.map_to_local(cell)
 
 func idle_state() -> void:
  animated_sprite.play("idle")
 
 func set_wander_state() -> void:
- var map_rid = nav_region.get_region_rid()
- var p: Vector2 = NavigationServer2D.region_get_random_point(map_rid, 1, false)
+#
+ var p: Vector2 = get_random_walkable_point()
+ print("x", p.x, "y: ", p.y)
  target = p
  nav.target_position = p
  state["wandering"] = true
@@ -34,9 +52,10 @@ func wander(delta : float) -> void:
  velocity = dir * speed
  nav.set_velocity(velocity)
 
-func _ready() -> void:
- nav.path_desired_distance = 100
- nav.target_desired_distance = 30
+#func _ready() -> void:
+ #nav.path_desired_distance = 100
+ #nav.target_desired_distance = 30
+ #nav.set_navigation_layer_value(0, 1)
 
 func _process(delta: float) -> void:
  label.text = "done: %s\nReached: %s\nReachable: %s\n" % [
