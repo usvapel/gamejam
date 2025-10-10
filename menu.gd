9e0aece21c3394fb6fe92fdda03939594cf9df40
jackie jan <zakaria.farah55@outlook.com>
2025-10-10 14:35:45 +0300

    controller support

--- menu.gd
+++ menu.gd
@@ -1,4 +1,5 @@
 extends Node2D
+
 @onready var fader = $"fade"
 @onready var anim = $"fade/AnimationPlayer"
 @onready var timer = $"fade/Timer2"
@@ -6,10 +7,29 @@ extends Node2D
 @onready var sfx_theme: AudioStreamPlayer = $sfx_theme
 @onready var sfx_press: AudioStreamPlayer = $sfx_press
 
-var btn = null
+@onready var start: Button = $Control/Start
+@onready var quit: Button = $Control/Quit
+
+var btn: String = ""
+
 func _ready():
 	sfx_theme.play()
-	
+	start.grab_focus() # Ensure keyboard/controller starts here
+
+func _unhandled_input(event):
+	if event.is_action_pressed("ui_accept"):
+		var focused = get_viewport().gui_get_focus_owner()
+		if focused:
+			focused.emit_signal("pressed")
+
+func _input(event):
+	if Input.is_action_just_pressed("controller_X"):
+		btn = "start"
+		sfx_press.play()
+		fader.show()
+		timer.start()
+		anim.play("fade_in")
+
 func _on_button_pressed() -> void:
 	btn = "start"
 	sfx_press.play()
@@ -20,9 +40,12 @@ func _on_button_pressed() -> void:
 func _on_quit_pressed() -> void:
 	sfx_press.play()
 	btn = "quit"
+	fader.show()
+	timer.start()
+	anim.play("fade_in")
 
 func _on_fade_timer() -> void:
 	if btn == "start":
 		get_tree().change_scene_to_file("res://main.tscn")
-	if btn == "quit":
+	elif btn == "quit":
 		get_tree().quit()
